d95bf39fa4c29c0c10f1fb9e5a98e22c06a76c5f
Jason Powell <jasonpowell87@gmail.com>
2017-06-20 11:08:04 -0400

    set up SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
         options: {
           presets: ['react', 'es2015']
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
